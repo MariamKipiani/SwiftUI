dba30b7262fff84e753503a2faea59e7023a1cbe
MariamKipiani <145837678+MariamKipiani@users.noreply.github.com>
2023-12-06 11:22:08 +0400

    Initial Commit

---
+++ SwiftUI-Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI-Basics
+//
+//  Created by user on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
